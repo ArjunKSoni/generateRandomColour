c34d2ce975d5b56e6a78f77981d802b466d65a87
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-04-22 15:15:51 +0530

    .

--- App.jsx
+++ App.jsx
@@ -12,20 +12,30 @@ export default function App() {
     let val={colour:"rgb("+r.toString()+","+g.toString()+","+b.toString()+")"}
     setColour(color.concat(val))
   }
+  const remove=()=>{
+    setColour([])
+  }
 
   return (
     <View className="flex-1 mt-10 items-center w-screen bg-white">
       <Text className="text-xl font-extrabold"><Text className="text-green-600">Generate</Text> random <Text className="text-red-600">Colour</Text></Text>
+      <View className="flex-1 flex-row justify-around w-screen h- p-5 mb-16 mt-2">
       <TouchableOpacity onPress={press}>
-        <Text className="p-5 bg-slate-900 mb-4 text-white rounded-lg mt-2">Generate</Text>
+        <Text className="p-5 text-center bg-slate-900 mb-4 w-36 h-16 text-white rounded-lg mt-2">Generate</Text>
       </TouchableOpacity>
+      <TouchableOpacity onPress={remove}>
+        <Text className="p-5 text-center bg-slate-900 mb-4 w-36 h-16 text-white rounded-lg mt-2">Reset</Text>
+      </TouchableOpacity>
+      </View>
 
+      <View className="justify-start mb-40">
       <FlatList
       data={color}
       renderItem={({item})=>{
         return <Text className="text-white rounded-xl m-2 p-4 text-center font-extrabold text-xl" style={{backgroundColor:item.colour,width:300}}>{item.colour}</Text>
       }}
       />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
